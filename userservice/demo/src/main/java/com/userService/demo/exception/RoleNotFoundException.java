3465942cdf86de1e5f9dff4c227d407014b68d1e
Sandeepmehra79 <83429487+Sandeepmehra79@users.noreply.github.com>
2024-04-15 04:28:35 +0530

    Delete userservice/demo directory

--- userservice/demo/src/main/java/com/userService/demo/exception/RoleNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.userService.demo.exception;
-
-import com.userService.demo.model.Role;
-
-public class RoleNotFoundException extends Exception{
-    public RoleNotFoundException(String message){
-        super(message);
-    }
-}
--- userservice/demo/src/main/java/com/userService/demo/model/SessionStatus.java
+++
@@ -1,6 +0,0 @@
-package com.userService.demo.model;
-
-public enum SessionStatus {
-    ACTIVE,
-    ENDED
-}
